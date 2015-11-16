c01180a7e7a789073d8fc10a0b5c5269616c43b6
TheMrcool212 <munirmohamadrani@gmail.com>
2015-11-15 20:40:11 -0500

    SystemUI: option to ignore the last app when clearing recents (1/2)

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3546,6 +3546,12 @@ public final class Settings {
          */
         public static final String SHOW_CLEAR_ALL_RECENTS = "show_clear_all_recents";
 
+	/**
+         * Whether to dismiss all recents tasks
+         * @hide
+         */
+        public static final String RECENTS_CLEAR_ALL_DISMISS_ALL = "recents_clear_all_dismiss_all";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
